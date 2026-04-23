03e50754bf0c68cc967475bcb7fa22695ec93f7e
MAKinTOIII <gdvvin@gmail.com>
2026-04-23 15:46:53 +0300

    Add lesson1.py - first Python script

---
+++ lesson1.py
@@ -0,0 +1,17 @@
+# Lesson 1 - first Python script
+print("Dmytro")
+print("Project Manager")
+print("I am learning Python to become a Technical PM.")
+
+# Lesson 2 & 3 - variables and f-strings
+my_name = "Dmytro"
+project_name = "Ensight"
+team_size = 35
+budget = 120000
+is_on_track = True
+
+print(f"Hello my name is {my_name}.")
+print(f"Project name: {project_name}.")
+print(f"Team size: {team_size} people.")
+print(f"Budget: ${budget}.")
+print(f"Project on track: {is_on_track}.")
